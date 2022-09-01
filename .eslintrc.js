4377b25bcf3b850f7f2be5d722f6640fdc22f52d
germansayago <german.sayago@gmail.com>
2022-09-01 12:12:34 -0300

    I installed packages and added the configuration to the project to establish a standard in the code

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+    es6: true,
+  },
+  extends: ['eslint:recommended', 'plugin:jsx-a11y/recommended', 'plugin:prettier/recommended', 'next', 'next/core-web-vitals'],
+  rules: {
+    semi: ['error', 'always'],
+    ' prettier/prettier': 0,
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton'],
+      },
+    ],
+  },
+};
---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  printWidth: 200,
+  tabWidth: 2,
+  useTab: false,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  endOfLine: 'auto',
+};
